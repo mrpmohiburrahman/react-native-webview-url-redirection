a36409a54bf54aed17e63940aed67eb40874d4c0
MD. MOHIBUR RAHMAN <mrpmohiburrahman@gmail.com>
2022-01-03 21:59:44 +0600

    implement url redirection

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useRef } from "react";
+import { StyleSheet, SafeAreaView, Text, View } from "react-native";
+import WebView from "react-native-webview";
 
-export default function App() {
+const App = () => {
+  const webRef = useRef(null);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <SafeAreaView style={styles.container}>
+      <View style={{ backgroundColor: "pink" }}>
+        <Text style={{ textAlign: "center", fontSize: 20 }}>
+          Native web page
+        </Text>
+      </View>
 
+      <View style={{ height: 400 }}>
+        <WebView
+          ref={webRef}
+          source={{ uri: "https://www.wikipedia.org/" }}
+          style={{ width: "100%", height: "100%" }}
+          onNavigationStateChange={(e) => {
+            const { url } = e;
+            if (url.includes("https://www.wikipedia.org/")) {
+              const newURL =
+                "https://www.google.com/search?client=firefox-b-d&q=react+native";
+              const redirectTo = 'window.location = "' + newURL + '"';
+              webRef.current.injectJavaScript(redirectTo);
+            }
+          }}
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#F5FCFF",
+    padding: 8,
   },
 });
